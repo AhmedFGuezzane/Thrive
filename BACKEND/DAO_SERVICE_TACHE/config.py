6a29dcd74d2345c9246a37b6a7c65bc80fde68d1
SLUniverse <dkicidy@gmail.com>
2025-06-19 11:15:48 -0400

    removed extra dao

--- BACKEND/DAO_SERVICE_TACHE/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    PORT = int(os.getenv("PORT", 5004))
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///taches.db")
